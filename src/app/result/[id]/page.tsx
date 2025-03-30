c54d9c38d7ff5bba61d6815030a26069a8698bea
Azer <azer.nagi.2005@gmail.com>
2025-03-31 03:17:36 +0400

    cx

--- src/app/result/[id]/page.tsx
+++ src/app/result/[id]/page.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { ResultDetails } from '@/app/result/[id]/components/ResultDetails';
 import { MainWrapper } from '@/widgets/MainWrapper';
 
-interface ResultsPageProps {
-  params: {
-    id: number;
-  };
-}
+export default async function ResultsPage({
+  params,
+}: {
+  params: Promise<{ id: number }>;
+}) {
+  const { id } = await params;
 
-export default async function ResultsPage({ params }: ResultsPageProps) {
   return (
     <MainWrapper>
-      <ResultDetails id={params.id} />
+      <ResultDetails id={id} />
     </MainWrapper>
   );
 }
