1bcc9c42dabca19cbbb12fa3bdccc3301291c925
Azer <azer.nagi.2005@gmail.com>
2025-04-28 22:38:27 +0400

    x

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,13 +1,16 @@
 'use client';
 
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Quiz } from '@/feature/Quiz';
 import { useSearchParams } from 'next/navigation';
 
 export default function QuizPage() {
   const searchParams = useSearchParams();
   const urlName = searchParams.get('q');
-  console.log(urlName);
 
-  return <Quiz questionCategory={urlName || ''} />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Quiz questionCategory={urlName || ''} />
+    </Suspense>
+  );
 }
